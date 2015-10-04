0c7e4b4f195a75f7b37dd28570e8af8602705100
Faron Bracy <signup@dreamersdesign.com>
2015-10-04 12:02:02 -0400

    Initial checkin of GoalMap sample application

--- RogueSharpRLNetSamples/Program.cs
+++ RogueSharpRLNetSamples/Program.cs
@@ -1,120 +1,216 @@
-﻿using RLNET;
+﻿using System.Collections.Generic;
+using RLNET;
 using RogueSharp;
+using RogueSharp.MapCreation;
 
 namespace RogueSharpRLNetSamples
 {
    public class Program
    {
-      // The screen height and width are in number of tiles
       private static readonly int _screenWidth = 50;
       private static readonly int _screenHeight = 50;
-      // The starting position for the player
-      private static int _playerX = 25;
-      private static int _playerY = 25;
+
       private static RLRootConsole _rootConsole;
       private static IMap _map;
+      private static IGoalMap _goalMap;
+      private static List<Point> _goals;
+      private static List<Point> _obstacles;
+      private static Point _start;
+      private static Path _path;
+      private static IReadOnlyCollection<Path> _allPaths;
+      private static bool _avoidGoals = false;
 
       public static void Main()
       {
-         // Use RogueSharp to create a new cave map the same size as the screen.
          _map = Map.Create( new CaveMapCreationStrategy<Map>( _screenWidth, _screenHeight, 45, 4, 3 ) );
-         // This must be the exact name of the bitmap font file we are using or it will error.
+         _goalMap = new GoalMap( _map );
+         _goals = new List<Point>();
+         _obstacles = new List<Point>();
+         _start = null;
+         _path = null;
+         _allPaths = null;
+
          string fontFileName = "terminal8x8.png";
-         // The title will appear at the top of the console window
          string consoleTitle = "RougeSharp RLNet Tutorial";
-         // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
          _rootConsole = new RLRootConsole( fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle );
-         // Set up a handler for RLNET's Update event
          _rootConsole.Update += OnRootConsoleUpdate;
-         // Set up a handler for RLNET's Render event
          _rootConsole.Render += OnRootConsoleRender;
-         // Begin RLNET's game loop
          _rootConsole.Run();
       }
 
-      // Event handler for RLNET's Update event
       private static void OnRootConsoleUpdate( object sender, UpdateEventArgs e )
       {
+         if ( _rootConsole.Mouse.GetLeftClick() )
+         {
+            SetStartLocation();
+         }
+         if ( _rootConsole.Mouse.GetRightClick() )
+         {
+            ToggleGoalAtMouseLocation();
+         }
+
          RLKeyPress keyPress = _rootConsole.Keyboard.GetKeyPress();
          if ( keyPress != null )
          {
-            // Check for the Up key press
-            if ( keyPress.Key == RLKey.Up )
+            if ( keyPress.Key == RLKey.O )
             {
-               // Check the RogueSharp map to make sure the Cell is walkable before moving
-               if ( _map.GetCell( _playerX, _playerY - 1 ).IsWalkable )
-               {
-                  // Update the player position
-                  _playerY--;
-               }
+               ToggleObstacleAtMouseLocation();
             }
-            // Repeat for the other directions
-            else if ( keyPress.Key == RLKey.Down )
+            else if ( keyPress.Key == RLKey.A )
             {
-               if ( _map.GetCell( _playerX, _playerY + 1 ).IsWalkable )
-               {
-                  _playerY++;
-               }
+               ToggleAvoidance();
             }
-            else if ( keyPress.Key == RLKey.Left )
+            else if ( keyPress.Key == RLKey.C )
             {
-               if ( _map.GetCell( _playerX - 1, _playerY ).IsWalkable )
-               {
-                  _playerX--;
-               }
-            }
-            else if ( keyPress.Key == RLKey.Right )
-            {
-               if ( _map.GetCell( _playerX + 1, _playerY ).IsWalkable )
-               {
-                  _playerX++;
-               }
+               _start = null;
+               _goalMap.ClearGoals();
+               _goals = new List<Point>();
+               _obstacles = new List<Point>();
+               _path = null;
+               _allPaths = null;
             }
          }
       }
 
-      // Event handler for RLNET's Render event
       private static void OnRootConsoleRender( object sender, UpdateEventArgs e )
       {
          _rootConsole.Clear();
 
-         // Use RogueSharp to calculate the current field-of-view for the player
-         _map.ComputeFov( _playerX, _playerY, 50, true );
-
          foreach ( var cell in _map.GetAllCells() )
          {
-            // When a Cell is in the field-of-view set it to a brighter color
-            if ( cell.IsInFov )
+            _map.SetCellProperties( cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true );
+            if ( cell.IsWalkable )
             {
-               _map.SetCellProperties( cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true );
-               if ( cell.IsWalkable )
-               {
-                  _rootConsole.Set( cell.X, cell.Y, RLColor.Gray, null, '.' );
-               }
-               else
-               {
-                  _rootConsole.Set( cell.X, cell.Y, RLColor.LightGray, null, '#' );
-               }
+               _rootConsole.Set( cell.X, cell.Y, RLColor.Gray, null, '.' );
             }
-            // If the Cell is not in the field-of-view but has been explored set it darker
-            else if ( cell.IsExplored )
+            else
             {
-               if ( cell.IsWalkable )
+               _rootConsole.Set( cell.X, cell.Y, RLColor.LightGray, null, '#' );
+            }
+         }
+
+         foreach ( Point point in _goals )
+         {
+            _rootConsole.SetBackColor( point.X, point.Y, RLColor.Yellow );
+         }
+
+         if ( _start != null )
+         {
+            _rootConsole.SetBackColor( _start.X, _start.Y, RLColor.Green );
+         }
+
+         if ( _allPaths != null )
+         {
+            foreach ( Path path in _allPaths )
+            {
+               foreach ( Cell cell in path.Steps )
                {
-                  _rootConsole.Set( cell.X, cell.Y, new RLColor( 30, 30, 30 ), null, '.' );
-               }
-               else
-               {
-                  _rootConsole.Set( cell.X, cell.Y, RLColor.Gray, null, '#' );
+                  if ( cell == path.Start || cell == path.End )
+                  {
+                     continue;
+                  }
+                  _rootConsole.SetBackColor( cell.X, cell.Y, RLColor.LightBlue );
                }
             }
          }
 
-         // Set the player's symbol after the map symbol to make sure it is draw
-         _rootConsole.Set( _playerX, _playerY, RLColor.LightGreen, null, '@' );
+         if ( _path != null )
+         {
+            foreach ( Cell cell in _path.Steps )
+            {
+               if ( cell == _path.Start || cell == _path.End )
+               {
+                  continue;
+               }
+               _rootConsole.SetBackColor( cell.X, cell.Y, RLColor.Blue );
+            }
+         }
 
-         // Tell RLNET to draw the console that we set
+         foreach ( Point point in _obstacles )
+         {
+            _rootConsole.SetBackColor( point.X, point.Y, RLColor.LightRed );
+         }
+
          _rootConsole.Draw();
+      }
+
+      private static void ToggleGoalAtMouseLocation()
+      {
+         int x = _rootConsole.Mouse.X;
+         int y = _rootConsole.Mouse.Y;
+         if ( x < 0 || x >= _screenWidth || y < 0 || y >= _screenHeight )
+         {
+            return;
+         }
+         Point mouseLocataion = new Point( x, y );
+         if ( _goals.Contains( mouseLocataion ) )
+         {
+            _goals.Remove( mouseLocataion );
+            _goalMap.RemoveGoal( x, y );
+         }
+         else
+         {
+            _goals.Add( mouseLocataion );
+            _goalMap.AddGoal( x, y, 0 );
+         }
+         if ( _start != null )
+         {
+            UpdateGoalMapPaths();
+         }
+      }
+
+      private static void SetStartLocation()
+      {
+         int x = _rootConsole.Mouse.X;
+         int y = _rootConsole.Mouse.Y;
+         if ( x < 0 || x >= _screenWidth || y < 0 || y >= _screenHeight )
+         {
+            return;
+         }
+         Point mouseLocataion = new Point( x, y );
+         _start = mouseLocataion;
+         UpdateGoalMapPaths();
+      }
+
+      private static void ToggleObstacleAtMouseLocation()
+      {
+         int x = _rootConsole.Mouse.X;
+         int y = _rootConsole.Mouse.Y;
+         if ( x < 0 || x >= _screenWidth || y < 0 || y >= _screenHeight )
+         {
+            return;
+         }
+         Point mouseLocataion = new Point( x, y );
+         if ( _obstacles.Contains( mouseLocataion ) )
+         {
+            _obstacles.Remove( mouseLocataion );
+         }
+         else
+         {
+            _obstacles.Add( mouseLocataion );
+         }
+         UpdateGoalMapPaths();
+      }
+
+      private static void UpdateGoalMapPaths()
+      {
+         _goalMap.ClearObstacles();
+         _goalMap.AddObstacles( _obstacles );
+         if ( _avoidGoals )
+         {
+            _path = _goalMap.FindPathAvoidingGoals( _start.X, _start.Y );
+         }
+         else
+         {
+            _path = _goalMap.FindPath( _start.X, _start.Y );
+         }
+         _allPaths = _goalMap.FindPaths( _start.X, _start.Y );
+      }
+
+      private static void ToggleAvoidance()
+      {
+         _avoidGoals = !_avoidGoals;
+         UpdateGoalMapPaths();
       }
    }
 }
